f59d6c6e7d61968f827386fee626d044402247d5
Ryusen9 <asratul02@gmail.com>
2025-09-17 12:16:28 +0600

    Update axios to version 1.12.2; add new products to products.json; enhance TopRated component to display top-rated products in a carousel

--- src/components/Product Pages/Top Rated/TopRated.tsx
+++ src/components/Product Pages/Top Rated/TopRated.tsx
@@ -8,17 +8,26 @@ import {
 
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const TopRated = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("/json/products.json").then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+
   return (
     <div className="w-full mt-6 flex flex-col items-center justify-center gap-4">
+      {/* Section Header */}
       <div>
         <p className="font-fancy text-2xl text-center uppercase">
           Top Rated Products
@@ -27,9 +36,49 @@ const TopRated = () => {
           Discover our most popular products, as rated by our customers.
         </p>
       </div>
-      <div className="w-full flex items-center justify-center mt-4">
-        <Carousel>
-          <CarouselContent></CarouselContent>
+
+      {/* Carousel */}
+      <div className="w-3/4 mx-auto flex items-center justify-center mt-4">
+        <Carousel className="w-full flex-1">
+          <CarouselContent>
+            {products.map((product, index) => (
+              <CarouselItem
+                key={index}
+                className={`basis-full sm:basis-1/2 lg:basis-1/3 flex items-center justify-center ${
+                  product.rating < 4 ? "hidden" : "block"
+                }`}
+              >
+                <Card className="h-80 flex flex-col">
+                  <CardHeader>
+                    <CardTitle className="text-sm line-clamp-1">
+                      {product.name}
+                    </CardTitle>
+                    <CardDescription className="text-xs">
+                      {product.brand}
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent className="flex-1 flex items-center justify-center">
+                    <img
+                      src={product.images[0]}
+                      alt={product.name}
+                      className="h-28 object-contain"
+                    />
+                  </CardContent>
+                  <CardFooter className="flex flex-col gap-1">
+                    <p className="text-sm font-semibold">
+                      ${product.discountPrice}
+                    </p>
+                    <p className="text-xs line-through text-gray-400">
+                      ${product.price}
+                    </p>
+                    <p className="text-xs text-yellow-500">
+                      ⭐ {product.rating}
+                    </p>
+                  </CardFooter>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
@@ -37,4 +86,5 @@ const TopRated = () => {
     </div>
   );
 };
+
 export default TopRated;
